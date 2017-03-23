37086cfa716c546ccd55507c3d025c16a0ba7a03
Juan Felipe Donado <pipe.donado@gmail.com>
2017-03-23 16:04:09 -0500

    Dashboard initial stages

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/index';
+import { DashboardComponent } from './dashboard/index';
 import { LoginComponent } from './login/index';
 import { RegisterComponent } from './register/index';
-import { AuthGuard } from './_guards/index';
+import { AuthGuard } from './_auth/index';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
