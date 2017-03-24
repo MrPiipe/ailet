c284715655b7b5f8243c1b2367a7516180421067
Juan Felipe Donado <pipe.donado@gmail.com>
2017-03-23 19:43:57 -0500

    New Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,95 @@
-import { NgModule }      from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
 
-// used to create fake backend
-import { fakeBackendProvider } from './_backend/index';
-import { MockBackend, MockConnection } from '@angular/http/testing';
-import { BaseRequestOptions } from '@angular/http';
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+import { ENV_PROVIDERS } from './environment';
+import { routing } from './app.routing';
 
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
+// App is our top level component
+import { App } from './app.component';
+import { AppState, InternalStateType } from './app.service';
+import { GlobalState } from './global.state';
+import { NgaModule } from './theme/nga.module';
+import { PagesModule } from './pages/pages.module';
 
-import { AlertComponent } from './_alert/index';
-import { AuthGuard } from './_auth/index';
-import { AlertService, AuthenticationService, UserService } from './_services/index';
-import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
-import { DashboardComponent } from './dashboard/index';
-import { StatisticsComponent } from './statistics/index';
+// Application wide providers
+const APP_PROVIDERS = [
+  AppState,
+  GlobalState
+];
 
+export type StoreType = {
+  state: InternalStateType,
+  restoreInputValues: () => void,
+  disposeOldHosts: () => void
+};
+
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
 @NgModule({
-  imports: [
+  bootstrap: [App],
+  declarations: [
+    App
+  ],
+  imports: [ // import Angular's modules
     BrowserModule,
-    FormsModule,
     HttpModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgaModule.forRoot(),
+    PagesModule,
     routing
   ],
-  declarations: [
-    AppComponent,
-    AlertComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    StatisticsComponent
-  ],
-  providers: [
-    AuthGuard,
-    AlertService,
-    AuthenticationService,
-    UserService,
-
-    // providers used to create fake backend
-    fakeBackendProvider,
-    MockBackend,
-    BaseRequestOptions
-  ],
-  bootstrap: [AppComponent]
+  providers: [ // expose our Services and Providers into Angular's dependency injection
+    ENV_PROVIDERS,
+    APP_PROVIDERS
+  ]
 })
 
-export class AppModule { }
+export class AppModule {
+
+  constructor(public appRef: ApplicationRef, public appState: AppState) {
+  }
+
+  hmrOnInit(store: StoreType) {
+    if (!store || !store.state) return;
+    console.log('HMR store', JSON.stringify(store, null, 2));
+    // set state
+    this.appState._state = store.state;
+    // set input values
+    if ('restoreInputValues' in store) {
+      let restoreInputValues = store.restoreInputValues;
+      setTimeout(restoreInputValues);
+    }
+    this.appRef.tick();
+    delete store.state;
+    delete store.restoreInputValues;
+  }
+
+  hmrOnDestroy(store: StoreType) {
+    const cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // save state
+    const state = this.appState._state;
+    store.state = state;
+    // recreate root elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // save input values
+    store.restoreInputValues = createInputTransfer();
+    // remove styles
+    removeNgStyles();
+  }
+
+  hmrAfterDestroy(store: StoreType) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,20 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
-import { DashboardComponent } from './dashboard/index';
-import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
-import { AuthGuard } from './_auth/index';
-import { StatisticsComponent } from './statistics/index';
-
-const appRoutes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'statistics', component: StatisticsComponent },
-  { path: 'dashboard', component: DashboardComponent },
-
-  // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+export const routes: Routes = [
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages/dashboard' }
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
